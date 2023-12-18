f8570290160a7ae6adc3b66117ec559017b56dd9
BOUNOU <djeey.osis@gmail.com>
2023-12-18 17:29:49 +0100

    creation server et connection avec la base de données MongoDB

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+mongoose
+  .connect(
+    "mongodb+srv://bounououssama:x7e3FIrHBmPsDsXU@cluster-vieux-grimoire.mhn5m6g.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+module.exports = app;
